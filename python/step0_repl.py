f0d2509e09edcb7d081311c927495b21af70dd3d
taygetea <taygetea@gmail.com>
2015-05-27 00:41:56 -0700

    let's start this fresh.

--- python/step0_repl.py
+++ python/step0_repl.py
@@ -1,29 +1,28 @@
-import sys, traceback
-import mal_readline
+def READ(input):
+    return input
 
-# read
-def READ(str):
-    return str
 
-# eval
-def EVAL(ast, env):
-        #print("EVAL %s" % printer._pr_str(ast))
-        return ast
+def EVAL(input):
+    return input
 
-# print
-def PRINT(exp):
-    return exp
 
-# repl
-def REP(str):
-    return PRINT(EVAL(READ(str), {}))
+def _PRINT(input):
+    return input
 
-# repl loop
-while True:
-    try:
-        line = mal_readline.readline("user> ")
-        if line == None: break
-        if line == "": continue
-        print(REP(line))
-    except Exception as e:
-        print("".join(traceback.format_exception(*sys.exc_info())))
+
+def rep(input):
+    return _PRINT(EVAL(READ(input)))
+
+
+def main():
+    while True:
+        try:
+            command = rep(raw_input("user> "))
+            print command
+        except EOFError:
+            print '\n'
+            quit()
+
+
+
+main()
